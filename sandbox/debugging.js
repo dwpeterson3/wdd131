eb8a4f02bbd536fc0ebaec7d6879852c0bb75d1c
dwpeterson3 <pet08043@byui.edu>
2024-05-18 23:24:28 -0400

    debugging commits

---
+++ sandbox/debugging.js
@@ -0,0 +1,16 @@
+const PI = 3.14;
+let area = 0
+
+function circleArea(radius) 
+{
+    let area = radius * radius * PI;
+    return area
+}
+
+area = circleArea(3)
+console.log("Area1:", area);
+
+area = circleArea(4)
+console.log("Area2:", area)
+
+
