b7898ec5b18d386ce4a65edb79e318761b4fb34d
dwpeterson3 <pet08043@byui.edu>
2024-06-28 19:04:17 -0400

    updated js list objects

--- sandbox/sorting.js
+++ sandbox/sorting.js
@@ -1,62 +1,71 @@
 const hikes = [
     {
-        "name": "Bechler Falls",
-        "imgSrc": "//byui-cit.github.io/cit261/examples/falls.jpg",
-        "imgAlt": "Image of Bechler Falls",
-        "distance": "3 miles",
-        "difficulty": "Easy",
-        "description":
-            "Beautiful short hike along the Bechler river to Bechler Falls",
-        "directions":
-            "Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road. Drive to the end of the Cave Falls road. There is a parking area at the trailhead."
+      name: "Bechler Falls",
+      stub: "bechler_falls",
+      imgSrc:
+        "https://wdd131.netlify.app/examples/hikes/images/bechler-falls.jpg",
+      imgAlt: "Image of Bechler Falls",
+      distance: "3 miles",
+      tags: ["Easy", "Yellowstone", "Waterfall"],
+      description:
+        "Beautiful short hike in Yellowstone along the Bechler river to Bechler Falls",
+      directions:
+        "Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road.Drive to the end of the Cave Falls road. There is a parking area at the trailhead.",
+      trailhead: [44.14457, -110.99781]
     },
     {
-        "name": "Teton Canyon",
-        "imgSrc": "//byui-cit.github.io/cit261/examples/falls.jpg",
-        "imgAlt": "Image of Teton Canyon",
-        "distance": "3 miles",
-        "difficulty": "Easy",
-        "description": "Beautiful short (or long) hike through Teton Canyon.",
-        "directions":
-            "Take Highway 33 East to Driggs. Turn left onto Teton Canyon Road. Follow that road for a few miles then turn right onto Staline Road for a short distance, then left onto Alta Road. Veer right after Alta back onto Teton Canyon Road. There is a parking area at the trailhead."
+      name: "Teton Canyon",
+      stub: "teton_canyon",
+      imgSrc: "https://wdd131.netlify.app/examples/hikes/images/teton-canyon.jpg",
+      imgAlt: "Image of Bechler Falls",
+      distance: "3 miles",
+      tags: ["Easy", "Tetons"],
+      description: "Beautiful short (or long) hike through Teton Canyon.",
+      directions:
+        "Take Highway 33 East to Driggs. Turn left onto Teton Canyon Road. Follow that road for a few miles then turn right onto Staline Raod for a short distance, then left onto Alta Road. Veer right after Alta back onto Teton Canyon Road. There is a parking area at the trailhead.",
+      trailhead: [43.75567, -110.91521]
     },
     {
-        "name": "Alaska Basin",
-        "imgSrc": "//byui-cit.github.io/cit261/examples/falls.jpg",
-        "imgAlt": "Image of Alaska Basin",
-        "distance": "17 miles",
-        "difficulty": "Difficult",
-        "description":
-            "Beautiful hike through Teton Canyon, up the Devils Staircase, to Alaska Basin",
-        "directions":
-            "Take Highway 33 East to Driggs. Turn left onto Teton Canyon Road. Follow that road for a few miles then turn right onto Staline Road for a short distance, then left onto Alta Road. Veer right after Alta back onto Teton Canyon Road. There is a parking area at the trailhead."
+      name: "Denanda Falls",
+      stub: "denanda_falls",
+      imgSrc:
+        "https://wdd131.netlify.app/examples/hikes/images/denanda-falls.jpg",
+      imgAlt: "Image of Bechler Falls",
+      distance: "7 miles",
+      tags: ["Moderate", "Yellowstone", "Waterfall"],
+      description: "Beautiful hike through Bechler meadows to Denanda Falls",
+      directions:
+        "Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road. Drive to until you see the sign for Bechler Meadows on the left. Turn there. There is a parking area at the trailhead.",
+      trailhead: [44.14974, -111.04564]
     },
     {
-        "name": "North Menan Butte",
-        "imgSrc": "//byui-cit.github.io/cit261/examples/falls.jpg",
-        "imgAlt": "Image of Menan Butte",
-        "distance": "3.4 miles",
-        "difficulty": "Moderate",
-        "description":
-            "Beautiful hike up and around the Menan Butte, an extinct volcano cone",
-        "directions":
-            "Take Main Street West out of Rexburg. Continue onto Highway 33. Travel west for about 8 miles until you see E Butte Rd on the South. Turn left and travel about 1 mile, turn left at your first opportunity onto Twin Butte Road. Follow that road for about 2 miles. The trailhead parking lot will be on the left"
+      name: "Coffee Pot Rapids",
+      stub: "coffee_pot",
+      imgSrc: "https://wdd131.netlify.app/examples/hikes/images/coffee-pot.jpg",
+      imgAlt: "Image of Bechler Falls",
+      distance: "2.2 miles",
+      tags: ["Easy"],
+      description:
+        "Beautiful hike along the Henry's Fork of the Snake River to a set of rapids.",
+      directions:
+        "Take Highway 20 north to Island Park. Continue almost to Mack's in. From Highway 20, turn west on Flatrock Road for 1 mile then turn off on Coffee Pot Road and travel one-half mile to the campground entrance road. There is a parking lot right outside the campground.",
+      trailhead: [44.49035, -111.36619]
     },
     {
-        "name": "Denanda Falls",
-        "imgSrc": "//byui-cit.github.io/cit261/examples/falls.jpg",
-        "imgAlt": "Image of Bechler Falls",
-        "distance": "7 miles",
-        "difficulty": "Moderate",
-        "description":
-            "Beautiful hike through Bechler meadows river to Denanda Falls",
-        "directions":
-            "Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road. Drive to until you see the sign for Bechler Meadows on the left. Turn there. There is a parking area at the trailhead."
+      name: "Menan Butte",
+      stub: "menan_butte",
+      imgSrc: "https://wdd131.netlify.app/examples/hikes/images/menan-butte.jpg",
+      imgAlt: "Image of Menan Butte",
+      distance: "3.4 miles",
+      tags: ["Moderate", "View"],
+      description:
+        "A steep climb to one of the largest volcanic tuff cones in the world. 3.4 miles is the full loop around the crater, can be shortened.",
+      directions:
+        "Take Highway 33 West out of Rexburg for about 8 miles. Turn left onto E Butte Road, the right onto Twin Butte road after about a mile. Follow that road for about 3 miles. You will see the parking lot/trailhead on the left.",
+      trailhead: [43.78555, -111.98996]
     }
-
-];
-const simpleList = ["oranges", "grapes", "lemons", "apples", "Bananas", "watermelons", "coconuts", "broccoli", "mango"];
-
+  ];
+   const simpleList = ["oranges", "grapes", "lemons", "apples", "Bananas", "watermelons", "coconuts", "broccoli", "mango"];
 simpleSort = simpleList.sort();
 
 function compareFunction(a, b) {
