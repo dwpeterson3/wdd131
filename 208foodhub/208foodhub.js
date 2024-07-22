fdb4c3e320e5b1f88e558d06464ff69e36dd91f2
dwpeterson3 <pet08043@byui.edu>
2024-07-22 09:46:51 -0400

    update for function

--- 208foodhub/208foodhub.js
+++ 208foodhub/208foodhub.js
@@ -1,10 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
+    setupHamburgerMenu();
+});
+
+
+function setupHamburgerMenu(){
     const hamburger = document.querySelector('.hamburger');
     const nav = document.querySelector('.nav');
 
         hamburger.addEventListener('click', () => {
         nav.classList.toggle('active');
     });
-});
-
+}
 
